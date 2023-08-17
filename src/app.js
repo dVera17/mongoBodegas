9d2ac8fb4d06a98c0ad48346ede5dfd2f42e4039
dVera17 <dsvera174@gmail.com>
2023-08-16 20:46:25 -0500

    feat: :sparkles: Se utilizan los endpoints para generar tokens y para bodega y productos
    Se utilizan los endpoints para generar tokens y para bodega y productos

--- src/app.js
+++ src/app.js
@@ -1,8 +1,17 @@
 import express from 'express';
 import { config } from "dotenv";
+import routerJWT from './routes/JWT.routes.js';
+import verifyToken from './middlewares/JWT/authToken.js';
+import routerBodegas from './routes/bodega.routes.js';
+import routerProducto from './routes/producto.routes.js';
 config();
 const app = express();
 app.set('port', process.env.PORT_SERVER);
 app.use(express.json());
+
+app
+    .use('/token', routerJWT)
+    .use('/api/bodegas', verifyToken, routerBodegas)
+    .use('/api/productos', verifyToken, routerProducto)
 
 export default app;
