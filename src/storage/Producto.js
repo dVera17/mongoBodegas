0c7052a15cec43eabae2613024ab1862e0f47fe9
dVera17 <dsvera174@gmail.com>
2023-08-16 20:33:55 -0500

    feat: :sparkles: Archivo transpilado
    Archivo transpilado

--- src/storage/Producto.js
+++ src/storage/Producto.js
@@ -8,6 +8,7 @@ var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 import { Expose } from 'class-transformer';
+import { IsDefined } from 'class-validator';
 export default class Producto {
     constructor(data) {
         Object.assign(this, data);
@@ -19,18 +20,22 @@ export default class Producto {
 }
 __decorate([
     Expose({ name: "id_prod" }),
+    IsDefined({ message: () => { throw { status: 406, message: "'id_prod' es requerido" }; } }),
     __metadata("design:type", Number)
 ], Producto.prototype, "id", void 0);
 __decorate([
     Expose({ name: "name_prod" }),
+    IsDefined({ message: () => { throw { status: 406, message: "'name_prod' es requerido" }; } }),
     __metadata("design:type", String)
 ], Producto.prototype, "nombre", void 0);
 __decorate([
     Expose({ name: "desc_prod" }),
+    IsDefined({ message: () => { throw { status: 406, message: "'desc_prod' es requerido" }; } }),
     __metadata("design:type", String)
 ], Producto.prototype, "descripcion", void 0);
 __decorate([
     Expose({ name: "status_prod" }),
+    IsDefined({ message: () => { throw { status: 406, message: "'status_prod' es requerido" }; } }),
     __metadata("design:type", Number)
 ], Producto.prototype, "estado", void 0);
 __decorate([
