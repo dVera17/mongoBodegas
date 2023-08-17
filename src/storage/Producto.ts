01253e8c0aba5bb932533aef83e8071104255059
dVera17 <dsvera174@gmail.com>
2023-08-16 20:33:34 -0500

    feat: :sparkles: Se corrige el dto para validar campos obligatorios
    Se corrige el dto para validar campos obligatorios

--- src/storage/Producto.ts
+++ src/storage/Producto.ts
@@ -2,26 +2,30 @@ import { Expose } from 'class-transformer';
 import { IsDefined } from 'class-validator';
 export default class Producto {
 
-    @Expose({name: "id_prod"})
+    @Expose({ name: "id_prod" })
+    @IsDefined({ message: () => { throw { status: 406, message: "'id_prod' es requerido" } } })
     id: number
-    @Expose({name: "name_prod"})
+    @Expose({ name: "name_prod" })
+    @IsDefined({ message: () => { throw { status: 406, message: "'name_prod' es requerido" } } })
     nombre: string
-    @Expose({name: "desc_prod"})
+    @Expose({ name: "desc_prod" })
+    @IsDefined({ message: () => { throw { status: 406, message: "'desc_prod' es requerido" } } })
     descripcion: string
-    @Expose({name: "status_prod"})
+    @Expose({ name: "status_prod" })
+    @IsDefined({ message: () => { throw { status: 406, message: "'status_prod' es requerido" } } })
     estado: number
-    @Expose({name: "created_by_prod"})
+    @Expose({ name: "created_by_prod" })
     created_by: number
-    @Expose({name: "update_by_prod"})
+    @Expose({ name: "update_by_prod" })
     update_by: number
-    @Expose({name: "created_at_prod"})
+    @Expose({ name: "created_at_prod" })
     created_at: string
-    @Expose({name: "updated_at_prod"})
+    @Expose({ name: "updated_at_prod" })
     updated_at: string
-    @Expose({name: "deleted_at_prod"})
+    @Expose({ name: "deleted_at_prod" })
     deleted_at: string
 
-    constructor(data:Partial<Producto>){
+    constructor(data: Partial<Producto>) {
         Object.assign(this, data)
         this.id = 0;
         this.nombre = "nombre";
